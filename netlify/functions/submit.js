c70466a2f6d0325f1ff201951f202b8af3f53af0
tylerwaypost <tyler@waypostsd.org>
2026-01-13 12:18:38 -0600

    Fix formatting of AIRTABLE_API_KEY
    Changed AIRTABLE_API_KEY to use single quotes.

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -1,4 +1,4 @@
-const AIRTABLE_API_KEY = patFlbergLmgbVjzY.63c492429097f437368effd8011539e20c22e0dec2ca1a2a2f9f873b2bef53a3
+const AIRTABLE_API_KEY = 'patFlbergLmgbVjzY.63c492429097f437368effd8011539e20c22e0dec2ca1a2a2f9f873b2bef53a3';
 const AIRTABLE_BASE_ID = 'apphCeBBITTuh5pb5';
 const AIRTABLE_TABLE_NAME = 'Staff Responses';
 
